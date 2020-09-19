a290d6fc8024ab1c766bc429f4fb707a9da648c0
unknown <sairaj2119@gmail.com>
2020-09-19 20:10:09 +0530

    safe now after this i will do footer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,21 +3,6 @@ import '../styles/Hero.css';
 import Nav from './Nav';
 import '../styles/Hero.css';
 import myPic from '../images/raj.png';
-import triangles from '../images/triangles.png';
-import img1 from '../images/1.png';
-import img2 from '../images/2.png';
-import img3 from '../images/3.png';
-import img4 from '../images/4.png';
-import img5 from '../images/5.png';
-import img6 from '../images/6.png';
-import img7 from '../images/7.png';
-import img8 from '../images/8.png';
-import img9 from '../images/9.png';
-import img10 from '../images/10.png';
-import img11 from '../images/11.png';
-import img12 from '../images/12.png';
-import img13 from '../images/13.png';
-import img14 from '../images/14.png';
 
 const Hero = () => {
   const trianglesRef = useRef();
@@ -31,7 +16,7 @@ const Hero = () => {
   return (
     <>
       <Nav />
-      <div onMouseMove={(e) => onMouseMove(e)} className="hero">
+      <div className="hero">
         <div className="hero__main">
           <div className="hero__content">
             <p className="hero__contentIntro">
@@ -54,13 +39,6 @@ const Hero = () => {
           </div>
         </div>
       </div>
-
-      <img
-        ref={trianglesRef}
-        className="hero__triangles"
-        src={triangles}
-        alt="triangles"
-      />
     </>
   );
 };
