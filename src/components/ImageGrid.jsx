3c6bbed0513bbeffbcf7c8aca1d397eb90307b08
unknown <sairaj2119@gmail.com>
2020-09-21 22:09:32 +0530

    replaced image and para with LightboxImg component

--- src/components/ImageGrid.jsx
+++ src/components/ImageGrid.jsx
@@ -5,30 +5,36 @@ import movieList from '../images/gallery/movieList.png';
 import FindYourDog from '../images/gallery/FindYourDog.png';
 import csClub from '../images/gallery/csClub.png';
 import reactAmazonClone from '../images/gallery/reactAmazonClone.png';
+import LightboxImg from './LightboxImg';
 
 const ImageGrid = () => {
   return (
     <div className="imageGrid">
       <div className="grid__container">
         <div className="grid__project">
-          <img src={reactAmazonClone} alt="" />
-          <p>This is a slack clone</p>
+          <LightboxImg img={reactAmazonClone} title={'This is a slack clone'} />
+          {/* <img src={reactAmazonClone} alt="" />
+          <p>This is a slack clone</p> */}
         </div>
         <div className="grid__project">
-          <img src={reactSlackClone} alt="" />
-          <p>This is a slack clone</p>
+          <LightboxImg img={reactSlackClone} title={'This is a slack clone'} />
+          {/* <img src={reactSlackClone} alt="" />
+          <p>This is a slack clone</p> */}
         </div>
         <div className="grid__project">
-          <img src={movieList} alt="" />
-          <p>This is a slack clone</p>
+          <LightboxImg img={movieList} title={'This is a slack clone'} />
+          {/* <img src={movieList} alt="" />
+          <p>This is a slack clone</p> */}
         </div>
         <div className="grid__project">
-          <img src={FindYourDog} alt="" />
-          <p>This is a slack clone</p>
+          <LightboxImg img={FindYourDog} title={'This is a slack clone'} />
+          {/* <img src={FindYourDog} alt="" />
+          <p>This is a slack clone</p> */}
         </div>
         <div className="grid__project">
-          <img src={csClub} alt="" />
-          <p>This is a slack clone</p>
+          <LightboxImg img={csClub} title={'This is a slack clone'} />
+          {/* <img src={csClub} alt="" />
+          <p>This is a slack clone</p> */}
         </div>
       </div>
     </div>
--- src/components/LightboxImg.jsx
+++ src/components/LightboxImg.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../styles/LightboxImg.css';
 
-const LightboxImg = ({ img }) => {
+const LightboxImg = ({ img, title }) => {
   return (
     <div className="lightboxImg">
       <img src={img} alt="" />
+      <p> {title} </p>
     </div>
   );
 };
