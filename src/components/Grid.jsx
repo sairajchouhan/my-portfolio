b182b93e5b715065f4bcce8b409a16e1dc5a10c4
unknown <sairaj2119@gmail.com>
2020-09-23 00:17:16 +0530

    light box effect is added

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,15 +1,53 @@
 import React from 'react';
 import GridItem from './GridItem';
-
 import '../styles/Grid.css';
-// These are images
+// These are thumbnail images
 import FindYourDog from '../images/gallery/FindYourDog.jpg';
 import movieList from '../images/gallery/movieList.jpg';
 import reactAmazonClone from '../images/gallery/reactAmazonClone.jpg';
 import reactSlackClone from '../images/gallery/reactSlackClone.jpg';
 import csClub from '../images/gallery/csClub.jpg';
+//  Light box images
+// ! Amazon lightbox images
+import cart from '../images/lightbox/reactAmazonClone/cart.jpg';
+import checkout from '../images/lightbox/reactAmazonClone/checkout.jpg';
+import orders from '../images/lightbox/reactAmazonClone/orders.jpg';
+import register from '../images/lightbox/reactAmazonClone/register.jpg';
+import signin from '../images/lightbox/reactAmazonClone/signin.jpg';
+// ! Slack lightbox images
+import auth from '../images/lightbox/reactSlackClone/auth.jpg';
+import chat from '../images/lightbox/reactSlackClone/chat.jpg';
+// ! Movie list
+import movieListL from '../images/lightbox/movieList/movieList.jpg';
+// ! Find your Dog
+import allBreeds from '../images/lightbox/FindYourDog/allBreeds.jpg';
+import dogs from '../images/lightbox/FindYourDog/dogs.jpg';
+import landing from '../images/lightbox/FindYourDog/landing.jpg';
+// ! csClub
+import about from '../images/lightbox/csClub/about.jpg';
+import blog from '../images/lightbox/csClub/blog.jpg';
+import contact from '../images/lightbox/csClub/contact.jpg';
+import events from '../images/lightbox/csClub/events.jpg';
+import faq from '../images/lightbox/csClub/faq.jpg';
+import gallery from '../images/lightbox/csClub/gallery.jpg';
+import home from '../images/lightbox/csClub/home.jpg';
+import people from '../images/lightbox/csClub/about.jpg';
 
 const Grid = () => {
+  const reactAmazonCloneLightbox = [cart, checkout, orders, register, signin];
+  const reactSlackCloneLightbox = [auth, chat];
+  const movieListLightbox = [movieListL];
+  const FindYourDogLightbox = [allBreeds, dogs, landing];
+  const csClubLightbox = [
+    about,
+    blog,
+    contact,
+    events,
+    faq,
+    gallery,
+    home,
+    people,
+  ];
   return (
     <div className="grid">
       <div className="grid__container">
@@ -19,6 +57,11 @@ const Grid = () => {
             title={'React Amazon Clone'}
             githubRepo={'https://github.com/sairaj2119/react-amazon-clone'}
             projectLink={'https://react--clone-b3bcc.web.app/'}
+            lightboxImages={reactAmazonCloneLightbox}
+            imageTitle={'The epic amazon clone'}
+            imageCaption={
+              'The clone is built using react which is the most rending framework now in 2020 and backend is served using firebase using firestore and cloud functions to implement payment gateway integration using stripe'
+            }
           />
         </div>
         <div className="grid__item">
@@ -27,6 +70,11 @@ const Grid = () => {
             title={'React Slack Clone'}
             githubRepo={'https://github.com/sairaj2119/react-slack-clone'}
             projectLink={'https://react-slack-clone-b655c.web.app/'}
+            lightboxImages={reactSlackCloneLightbox}
+            imageTitle={'Slack Clone'}
+            imageCaption={
+              'The clone is built using react which is the most rending framework now in 2020 and backend is served using firebase using firestore'
+            }
           />
         </div>
         <div className="grid__item">
@@ -35,6 +83,11 @@ const Grid = () => {
             title={'React Context API Usecase'}
             githubRepo={'https://github.com/sairaj2119/react-context-app'}
             projectLink={'https://sairaj2119.github.io/react-context-app/'}
+            lightboxImages={movieListLightbox}
+            imageTitle={'Movile list'}
+            imageCaption={
+              'This is a tiny project which uses react context API to make a simple movie list app where you can add and remove movies'
+            }
           />
         </div>
         <div className="grid__item">
@@ -43,6 +96,11 @@ const Grid = () => {
             title={'Find Your Dog'}
             githubRepo={'https://github.com/sairaj2119/FingYourDog'}
             projectLink={'https://sairaj2119.github.io/FingYourDog/'}
+            lightboxImages={FindYourDogLightbox}
+            imageTitle={'Find Your Dog'}
+            imageCaption={
+              'This is a little site that uses fetch api and gets some images of cute dogiees 🐕🐶🐕‍🦺🐩'
+            }
           />
         </div>
         <div className="grid__item">
@@ -51,6 +109,13 @@ const Grid = () => {
             title={'Computer Science Club Website'}
             githubRepo={'https://github.com/YogaVicky/CS-Club-IIITDM-Website'}
             projectLink={'https://yogavicky.github.io/CS-Club-IIITDM-Website/'}
+            lightboxImages={csClubLightbox}
+            imageTitle={
+              'IIITDM Kancheepuram, India, Computer Science club webiste'
+            }
+            imageCaption={
+              'CS club official website build on HTML, CSS, JavaScript and bootstrap'
+            }
           />
         </div>
       </div>
--- src/components/GridItem.jsx
+++ src/components/GridItem.jsx
@@ -1,27 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaGithub } from 'react-icons/fa';
 import { FaExternalLinkAlt } from 'react-icons/fa';
 import '../styles/GridItem.css';
+// react-lightbox
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
-const GridItem = ({ img, title, githubRepo, projectLink }) => {
+const GridItem = ({
+  img,
+  title,
+  githubRepo,
+  projectLink,
+  lightboxImages,
+  imageTitle,
+  imageCaption,
+}) => {
+  const images = lightboxImages;
+  const [photoIndex, setPhotoIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <>
-      <img src={img} alt={title} />
-      <div className="des">
-        <a
-          href={githubRepo}
-          style={{ fontSize: '1.5em' }}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <FaGithub />
-        </a>
-        <p>{title}</p>
-        <a href={projectLink} target="_blank" rel="noopener noreferrer">
-          <FaExternalLinkAlt />
-        </a>
+    <div>
+      <div onClick={() => setIsOpen(true)}>
+        <img src={img} alt={title} />
+        <div className="des">
+          <a
+            href={githubRepo}
+            style={{ fontSize: '1.5em' }}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaGithub />
+          </a>
+          <p>{title}</p>
+          <a href={projectLink} target="_blank" rel="noopener noreferrer">
+            <FaExternalLinkAlt />
+          </a>
+        </div>
       </div>
-    </>
+      {isOpen && (
+        <Lightbox
+          imageCaption={imageCaption}
+          imageTitle={imageTitle}
+          mainSrc={images[photoIndex]}
+          nextSrc={images[(photoIndex + 1) % images.length]}
+          prevSrc={images[(photoIndex + images.length - 1) % images.length]}
+          onCloseRequest={() => setIsOpen(false)}
+          onMovePrevRequest={() =>
+            setPhotoIndex((photoIndex + images.length - 1) % images.length)
+          }
+          onMoveNextRequest={() =>
+            setPhotoIndex((photoIndex + 1) % images.length)
+          }
+        />
+      )}
+    </div>
   );
 };
 
